ff4ae6609d852c94cb22ec4b0f2895de1a64a54d
nikhiljangra03 <nikhil16951@gmail.com>
2026-06-16 06:14:01 +0000

    declare some variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+/*
+    variable:- are the container which are use to store values ot data inside it.
+    types: let, var, const
+*/
+
+let name = "Nikhil" // can be change
+const rollNo = 17  //can't be able to change value
+var age = 22
+/* 
+    prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([name, rollNo, age]);
